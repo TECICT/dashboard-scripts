235160f8b408e65b79c6f99cb8bf2db748ef5bad
Laurens Allart <TECICT-Laurens@github.com>
2018-07-17 13:34:53 +0200

    first commit with dashboard scripts

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo forever stop /home/pi/apps/dashboard-node/server.js
+sudo forever stop /home/pi/apps/dashboard-angular/server.js
+sudo killall node
+DISPLAY=:0;xdotool mousemove 0 300
+cd /home/pi/apps/dashboard-node/
+sudo forever server.js &> /home/pi/logs/node_log.txt &
+echo "node server started"
+cd /home/pi/apps/dashboard-angular/
+sudo forever server.js &> /home/pi/logs/angular_log.txt &
+echo "angular server started"
+sleep 5s
+chromium-browser --disable-infobars --start-fullscreen --noerrdialogs --user-data-dir --disable-session-restore --disable-session-crashed-bubble --disable-web-security --restore-last-session http://localhost:80 &
+echo "display started"
+sleep 10s
+DISPLAY=:0;xdotool mousemove 1905 14 click 1
+
+
